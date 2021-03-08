750f335a8b8de90e2de68b5e8b663aeeb6839ba7
softpaper95 <minho100227@likelion.org>
2021-03-08 12:01:05 +0900

    make slide_btn and unfold slides

--- js/names_slide.js
+++ js/names_slide.js
@@ -1,2 +1,25 @@
-const f_names = document.getElementsByClassName('names');
-console.log(f_names.length)
+const n_slide_box = document.querySelector('.names_box');
+const n_slides = document.getElementsByClassName('names');
+const n_len = n_slides.length;
+
+
+
+
+
+
+
+//슬라이드 펼치기
+function unfold(){
+    for(i=0; i<n_len; i++){
+        n_slides[i].style.left=i*320+'px';
+    }
+};
+// function ex() {
+//     for(i=0; i<n_len; i++){
+//         f_names[i].style.backgroundColor='red';
+//     }
+// };
+
+
+unfold();
+// console.log(n_slide_box)
